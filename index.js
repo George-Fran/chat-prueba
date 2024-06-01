70dd286f07d91bf71634417a6230c49b5e9a738a
George-Fran <george.gutierrez@tecsup.edu.pe>
2024-05-31 20:31:46 -0500

    aea

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -11,22 +13,19 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Inicio' });
 });
 
-
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
   console.log('Alguien se conecto');
-  socket.on('chat message', function(mensaje) {
-      console.log('recibi: ' + mensaje);
-      io.emit('recibido', {
-          date: new Date(),
-          txt: mensaje
-      });
+  socket.on('chat message', (mensaje) => {
+    console.log('recibi: ' + mensaje);
+    io.emit('recibido', {
+      date: new Date(),
+      txt: mensaje
+    });
   });
 });
 
-
 app.use(express.static('public'));
 
-
-http.listen(port, function() {
+server.listen(port, () => {
   console.log('Listen on http://localhost:' + port);
 });
